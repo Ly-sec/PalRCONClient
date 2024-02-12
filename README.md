34f814e4a747ccf5549ec2a962829f4775388178
Ly-sec <cirada@t-online.de>
2024-02-12 15:57:32 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+This is a simple to use RCON client specifically made for the game Palworld.
+It is written in NodeJS and uses async functions.
+
+`palrconclient` is quite early in it's developement. Currently it features most rcon commands that Palworld has to offer. Oh did I mention that it also has a command queue? Makes your life much easier when you intend to send multiple commands at once.
+
+Oh, it also supports using multiple rcon connections at once, best used if you have multiple Palworld servers that you need to manage at the same time.
+
+# Usage
+```js
+const { PalRCONClient } = require('palrconclient')
+
+const rconClient1 = new PalRCONClient('IP', 25575, 'PASSWORD', {
+    onData: (data) => {
+        console.log('Connection 1 data:\n', data.response)
+    },
+    onEnd: () => {
+        console.log('Connection 1 closed.');
+    },
+    onError: (err) => {
+        console.error('Connection 1 error:', err)
+    },
+});
+
+// Either define which client should be used or use 'all' to send it to all clients.
+/**
+PalRCONClient.Send(rconClient1, "RCON COMMAND") //Run commands that are not specified (or implemented yet)
+PalRCONClient.Broadcast(rconClient1, "Random broadcast text") //Automatically converts spaces to "_"
+PalRCONClient.Save(rconClient1) // Saves the server
+PalRCONClient.Shutdown(rconClient1, "20", "Shutdown Message") // Shuts the server down after X seconds with message
+PalRCONClient.ShowPlayers(rconClient1) // Displays the players playing right now
+PalRCONClient.Kick(rconClient1, "SteamId") // Kicks player (steamId)
+PalRCONClient.Ban(rconClient1, "SteamId") // Bans player (steamId)
+*/
+```
+
+As soon as I have more features, I will start editing this readme.
+greetings, Lysec <3
---
+++ index.js
@@ -0,0 +1,290 @@
+const net = require('net');
+
+class PalRCONClient {
+
+  static connections = [];
+
+  constructor(serverIP, serverPort, password, options = {}) {
+    const { onEnd, onError, onData } = options;
+
+    this.serverIP = serverIP;
+    this.serverPort = serverPort;
+    this.password = password;
+    this.onEnd = onEnd;
+    this.onError = onError;
+    this.onData = onData;
+
+    this.socket = null;
+    this.authenticated = false;
+    this.commandQueue = [];
+    this.connectionId = `${serverIP}:${serverPort}`;
+    this.onEndLogged = false; // New flag to track if onEnd event has been logged
+
+    PalRCONClient.connections.push(this);
+  }
+
+  connect() {
+    this.socket = net.connect({ port: this.serverPort, host: this.serverIP }, () => {
+      console.log(`Connected to Palworld server at ${this.serverIP}:${this.serverPort}`);
+
+      const size = 14 + this.password.length;
+      const handshakePacket = Buffer.alloc(size);
+      handshakePacket.writeInt32LE(size, 0);
+      handshakePacket.writeInt32LE(0, 4);
+      handshakePacket.writeInt32LE(3, 8);
+      handshakePacket.write(this.password, 12, 'ascii');
+
+      // Check if the socket is still open before writing
+      if (this.socket && !this.socket.destroyed) {
+        this.socket.write(handshakePacket);
+      } else {
+        console.error('Attempted to write to a closed socket during connection.');
+      }
+    });
+
+    this.socket.on('data', (data) => {
+      this.handleData(data);
+    });
+
+    this.socket.on('end', () => {
+      console.log(`Connection ${this.connectionId} closed.`);
+      if (!this.onEndLogged && this.onEnd && typeof this.onEnd === 'function') {
+        try {
+          this.onEnd();
+          this.onEndLogged = true; // Set the flag to true after logging onEnd
+        } catch (err) {
+          console.error('Error in onEnd handler:', err);
+        }
+      }
+    });
+
+    this.socket.on('error', (err) => {
+      console.error(`Socket error in connection ${this.connectionId}:`, err.message);
+      if (this.onError && typeof this.onError === 'function') {
+        try {
+          this.onError(err);
+        } catch (err) {
+          console.error('Error in onError handler:', err);
+        }
+      }
+    });
+
+    this.socket.on('close', (hadError) => {
+      if (hadError) {
+        console.error(`Connection ${this.connectionId} closed due to an error.`);
+      }
+
+      if (this.onEnd && typeof this.onEnd === 'function') {
+        try {
+          this.onEndLogged = false; // Reset the flag for the next connection
+          this.onEnd();
+        } catch (err) {
+          console.error('Error in onEnd handler:', err);
+        }
+      }
+    });
+  }
+
+  handleData(data) {
+    const size = data.readInt32LE(0);
+    const requestId = data.readInt32LE(4);
+    const responseType = data.readInt32LE(8);
+    const payloadBuffer = data.slice(12);
+    const printablePayload = payloadBuffer.toString('utf-8').replace(/[^\x20-\x7E\u{4E00}-\u{9FFF}\u{3040}-\u{309F}\u{30A0}-\u{30FF}\u{AC00}-\u{D7AF}\u{0400}-\u{04FF}]/ug, '');
+
+    if (responseType === 2) {
+      this.authenticated = true;
+      console.log(`Authenticated to ${this.connectionId}`);
+      this.sendQueuedCommands();
+    } else {
+      if (this.onData && typeof this.onData === 'function') {
+        this.onData({ size, requestId, responseType, response: printablePayload });
+      }
+    }
+  }
+
+  async sendCommand(command) {
+    return new Promise((resolve, reject) => {
+      // Check if the socket is connected and authenticated
+      if (this.socket && !this.socket.destroyed && this.authenticated) {
+        const commandBuffer = Buffer.from(command, 'utf-8');
+        const size = 14 + commandBuffer.length;
+
+        const commandPacket = Buffer.alloc(size);
+        commandPacket.writeInt32LE(size, 0);
+        commandPacket.writeInt32LE(1, 4);
+        commandPacket.writeInt32LE(2, 8);
+        commandBuffer.copy(commandPacket, 12);
+
+        commandPacket.writeInt16LE(0, size - 2);
+
+        // Send the command
+        this.socket.write(commandPacket, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      } else if (!this.socket || this.socket.destroyed) {
+        // If the socket is not connected or destroyed, queue the command and attempt to reconnect
+        this.queueCommand(command);
+        this.reconnect();
+        resolve(); // Resolve immediately for asynchronous compatibility
+      } else {
+        // If the socket is still connecting, queue the command and wait for the 'connect' event
+        this.queueCommand(command);
+        this.socket.once('connect', () => {
+          // Ensure the socket is authenticated before sending any queued commands
+          if (this.authenticated) {
+            this.sendQueuedCommands();
+          }
+          resolve(); // Resolve after connection is established
+        });
+      }
+    });
+  }
+
+  reconnect() {
+    // Check if a reconnection attempt is already in progress
+    if (!this.reconnecting) {
+      this.reconnecting = true;
+
+      // Disconnect the current socket
+      this.disconnect();
+
+      // Attempt to reconnect after 5 second delay
+      setTimeout(() => {
+        this.connect();
+        this.reconnecting = false;
+      }, 5000);
+    }
+  }
+
+  queueCommand(command) {
+    this.commandQueue.push(command);
+  }
+
+  sendQueuedCommands() {
+    while (this.commandQueue.length > 0) {
+      this.sendCommand(this.commandQueue.shift());
+    }
+  }
+
+  disconnect() {
+    console.log(`Disconnecting ${this.connectionId}`);
+    if (this.socket) {
+      this.socket.end();
+      this.socket = null;
+    }
+  }
+
+  static async Send(target, command) {
+    if (target === 'all') {
+      const sendPromises = PalRCONClient.connections.map((instance) => PalRCONClient.send(instance, command));
+      return Promise.all(sendPromises);
+    } else {
+      const instance = PalRCONClient.connections.find((client) => client.connectionId === target);
+      if (instance) {
+        if (!instance.authenticated) {
+          instance.connect();
+        }
+        return instance.sendCommand(command);
+      } else {
+        throw new Error(`Instance with identifier '${target}' not found.`);
+      }
+    }
+  }
+
+  static async Broadcast(target, message) {
+    if (target === 'all') {
+      const broadcastPromises = PalRCONClient.connections.map((instance) => PalRCONClient.broadcast(instance, command));
+      return Promise.all(broadcastPromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      const broadcastMessage = `${message.replace(/ /g, "_")}`;
+      return instance.sendCommand(`Broadcast ${broadcastMessage}`);
+    }
+  }
+
+  static async Save(target) {
+    if (target === 'all') {
+      const savePromises = PalRCONClient.connections.map((instance) => PalRCONClient.save(instance));
+      return Promise.all(savePromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      return instance.sendCommand("save");
+    }
+  }
+
+  static async Shutdown(target, time = '0', message = '') {
+    if (target === 'all') {
+      const savePromises = PalRCONClient.connections.map((instance) => PalRCONClient.save(instance));
+      return Promise.all(savePromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      const shutdownMessage = `${message.replace(/ /g, "_")}`;
+      return instance.sendCommand(`Shutdown ${time} ${shutdownMessage}`);
+    }
+  }
+
+  static async ShowPlayers(target) {
+    if (target === 'all') {
+      const savePromises = PalRCONClient.connections.map((instance) => PalRCONClient.save(instance));
+      return Promise.all(savePromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      return instance.sendCommand("ShowPlayers");
+    }
+  }
+
+  static async Kick(target, steamId) {
+    if (target === 'all') {
+      const savePromises = PalRCONClient.connections.map((instance) => PalRCONClient.save(instance));
+      return Promise.all(savePromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      return instance.sendCommand(`Kick ${steamId}`);
+    }
+  }
+
+  static async Ban(target, steamId) {
+    if (target === 'all') {
+      const savePromises = PalRCONClient.connections.map((instance) => PalRCONClient.save(instance));
+      return Promise.all(savePromises);
+    } else {
+      const instance = target;
+      if (!instance.authenticated) {
+        instance.connect();
+      }
+      return instance.sendCommand(`Ban ${steamId}`);
+    }
+  }
+}
+
+PalRCONClient.connections = [];
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  });
+  
+  process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+  });
+
+module.exports = PalRCONClient;
